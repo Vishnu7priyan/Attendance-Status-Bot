4afe14c42e7b365d278badf241bc5e94c43819f3
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-01 20:34:22 +0530

    Create sel.py

---
+++ sel.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import requests
+_TOKEN = "Bot Token HERE"
+
+
+chrome_options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options=chrome_options)
+    
+
