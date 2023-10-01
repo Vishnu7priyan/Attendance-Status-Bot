e1873040c3b84a225470dcde5087d8e2dd7eeb4f
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-10-01 20:34:44 +0530

    Update sel.py

--- sel.py
+++ sel.py
@@ -7,4 +7,11 @@ _TOKEN = "Bot Token HERE"
 chrome_options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=chrome_options)
     
+def send_photo(chat_id, image_path, image_caption=""):
+    data = {"chat_id": chat_id, "caption": image_caption}
+    url = f'https://api.telegram.org/bot{_TOKEN}/sendPhoto'
+    with open(image_path, "rb") as image_file:
+        ret = requests.post(url, data=data, files={"photo": image_file})
+        print(ret.text)
+    return ret.json()
 
